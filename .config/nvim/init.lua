1cc78807a75894e6efe8f8619efa10f93fdfc31a
Kyle Just <kjjust7@gmail.com>
2024-05-02 15:09:14 -0700

    Update misc files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+if vim.g.neovide then
+    require("config.lazy")
+    vim.o.guifont = "Hack Nerd Font Mono"
+    vim.g.neovide_fullscreen = false
+    vim.g.neovide_cursor_smooth_blink = true
+    vim.g.neovide_cursor_vfx_mode = "ripple"
+end
+
--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+-- Example:
+-- vim.keymap.set("n", "Y", "y$", { desc = "Yank to end of line" })
